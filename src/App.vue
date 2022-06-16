4642bedb0ffbeb252565737afd791e7094fe2472
XINYUAN CHEN <2766799927@qq.com>
2022-06-16 10:34:34 +0800

    :tada: project Init

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <router-view />
+    <MonacoEditor
+      width="800"
+      height="500"
+      theme="vs-dark"
+      language="javascript"
+      :options="options"
+      @change="onChange"
+    ></MonacoEditor>
+  </div>
+</template>
+<script>
+import MonacoEditor from 'monaco-editor-vue'
+export default {
+  name: 'App',
+  components: {
+    MonacoEditor,
+  },
+  data() {
+    return {
+      options: {
+        //Monaco Editor Options
+      },
+    }
+  },
+  methods: {
+    onChange(value) {
+      console.log(value)
+    },
+  },
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
+      {
+        // Languages are loaded on demand at runtime
+        languages: ['json', 'javascript', 'html', 'xml'],
+      },
+    ])
+  },
+})
